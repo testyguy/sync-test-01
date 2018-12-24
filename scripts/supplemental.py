227bbfd9c49def1b135ad9b5ca8e6385411f6535
cwdunkerly <cwdunkerly@gmail.com>
2018-12-24 07:45:46 -0800

    initial commit

---
+++ scripts/supplemental.py
@@ -0,0 +1,6 @@
+def hello(name):
+    print("hello " + name + " have a good day")
+    print("this is my second attempt")
+
+
+
---
+++ test.py
@@ -0,0 +1,18 @@
+from supplemental import hello
+
+
+print("edit 01")
+
+
+def edit02():
+    x = 10
+    print(x)
+
+
+def edit03(var):
+    print(var)
+
+
+edit02()
+edit03(99)
+hello("joe")
